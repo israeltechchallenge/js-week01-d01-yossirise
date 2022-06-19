de1c7a51a6f6df1bd3f70f3fbddb87bbabbe25c5
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-19 15:27:05 +0300

    Solve exercise 5

--- D01/EX05/index.js
+++ D01/EX05/index.js
@@ -1,5 +1,21 @@
-function moduloEx5() {
-  //your code
+function moduloEx5(num1, num2) {
+  let comparisonResult, moduloResult, loggedString;
+
+  if (num1 < num2) {
+    comparisonResult = "less than";
+  } else if (num1 > num2) {
+    comparisonResult = "bigger than";
+    moduloResult = num1 % num2;
+  } else {
+    comparisonResult = "equal to";
+  }
+
+  loggedString = `The number ${num1} is ${comparisonResult} ${num2}`;
+  if (moduloResult) {
+    loggedString += `. But the modulo of ${num1} % ${num2} is ${moduloResult}`;
+  }
+
+  console.log(loggedString);
 }
 
 // Do not remove or change this line, or the tests won't work
