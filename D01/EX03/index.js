458df843dc6155edc1f0cc50c0b9ef91fd3d4d07
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-19 15:12:03 +0300

    Solve exercise 3

--- D01/EX03/index.js
+++ D01/EX03/index.js
@@ -1,5 +1,15 @@
-function compareNumbers() {
-  //your code
+function compareNumbers(compareNum, toNum) {
+  let comparisonResult;
+
+  if (compareNum < toNum) {
+    comparisonResult = "less than";
+  } else if (num > toNum) {
+    comparisonResult = "bigger than";
+  } else {
+    comparisonResult = "equal to";
+  }
+
+  console.log(`The number ${compareNum} is ${comparisonResult} ${toNum}`);
 }
 
 // Do not remove or change this line, or the tests won't work
