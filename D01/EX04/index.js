e6533c291d57abefd13044129fae348e771a81d6
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-19 15:23:42 +0300

    Solve exercise 4

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,5 +1,19 @@
-function modulo() {
-  //your code
+function modulo(num1, num2) {
+  let comparisonResult, moduloResult;
+
+  if (num1 < num2) {
+    comparisonResult = "less than";
+  } else if (num1 > num2) {
+    comparisonResult = "bigger than";
+    moduloResult = num1 % num2;
+  } else {
+    comparisonResult = "equal to";
+  }
+
+  console.log(`The number ${num1} is ${comparisonResult} ${num2}`);
+  if (moduloResult) {
+    console.log(`The modulo of ${num1} % ${num2} is ${moduloResult}`);
+  }
 }
 
 // Do not remove or change this line, or the tests won't work
