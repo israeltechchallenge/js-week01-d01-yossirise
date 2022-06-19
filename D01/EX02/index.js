07ffe59ae42e7be98a78b9c26be4a16b89f9c6c3
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-19 15:07:17 +0300

    Solve exercise 2

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,5 +1,15 @@
-function isBiggerThan() {
-  //your code
+function isBiggerThan(num) {
+  let compareTo4;
+
+  if (num < 4) {
+    compareTo4 = "less than";
+  } else if (num > 4) {
+    compareTo4 = "bigger than";
+  } else {
+    compareTo4 = "equal to";
+  }
+
+  console.log(`The number ${num} is ${compareTo4} 4`);
 }
 
 // Do not remove or change this line, or the tests won't work
