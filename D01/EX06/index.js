b8680ebbf18a5dd192b8a58d67316d2ef2344e26
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-19 16:12:15 +0300

    Solve exercise 6

--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,3 +1,29 @@
+main();
+
+function main() {
+  const number = 7,
+    comparer = 5;
+
+  const output = document.querySelector("#output");
+  output.textContent = moduloEx6(number, comparer);
+}
+
 function moduloEx6(number, comparer) {
-  //your code
+  let comparisonResult, moduloResult, resultString;
+
+  if (number < comparer) {
+    comparisonResult = "less than";
+  } else if (number > comparer) {
+    comparisonResult = "bigger than";
+    moduloResult = number % comparer;
+  } else {
+    comparisonResult = "equal to";
+  }
+
+  resultString = `The number ${number} is ${comparisonResult} ${comparer}`;
+  if (moduloResult) {
+    resultString += `. But the modulo of ${number} % ${comparer} is ${moduloResult}`;
+  }
+
+  return resultString;
 }
